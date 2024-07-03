ddb6b10ede3182c42b21d3636bf67bf19249deba
shahar-cohen1-1 <shachar.cohen4@mail.huji.ac.il>
2024-07-03 22:05:06 +0300

    preprocessing files

---
+++ passengers_up_preprocessing.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import numpy as np
+
+def remove_rows(df: pd.DataFrame) -> pd.DataFrame:
+
+def preprocessing(df: pd.DataFrame) -> pd.DataFrame:
+    # todo
+    # how many passengers on the bus
+
+
+    # total time at station (+noise?)
+    # distance between stations
+    # is between cities
+    # average people in this station (in total) - shows how busy is this area
+    # how many lines stop at this station
+    # how many alternative lines are there?
+    # how much time have passed since the last line
+    # trip duration
+    # merging same stations that have different station id
+    # outlier removal
+    #
+    # door closing time before arrival time
+    # negative
+    # categorial features -
+    # hours range
+    # is masof
